e64fc7f64e5099f2360f32144a7c503ea6d71076
Cristo González <alu0100694987@ull.edu.es>
2014-02-02 15:42:45 +0000

    Página del proyecto - Conversor de temperaturas

---
+++ temperature.js
@@ -0,0 +1,36 @@
+
+"use strict"; // ECMAScript 5 strict mode
+
+function calculate() {
+  
+  var result;
+  var original = document.getElementById("original");
+  var temp = original.value;
+  var regexp = /([+-]?\d+(?:\.\d+)?(?:[eE][-+]?\d+)?)\s*([cCfF])/;
+  
+  var m = temp.match(regexp);
+  
+  if (m) {
+    
+    var num = m[1];
+    var type = m[2];
+    num = parseFloat(num);
+    
+    if (type == 'c' || type == 'C') {
+      result = (num * 9/5)+32;
+      result = result + " ºF";
+    }
+    
+    else {
+      result = (num - 32)*5/9;
+      result = result + " ºC";
+    }
+    
+    converted.innerHTML = result;
+  }
+  
+  else {
+    converted.innerHTML = "Error: Try something like '-4.2C' instead";
+  }
+  
+}
